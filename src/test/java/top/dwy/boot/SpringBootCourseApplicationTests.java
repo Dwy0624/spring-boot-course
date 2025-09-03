9ca8dc77fb6097942ecb0ca558e45e1ab33158e2
dwy <1554642647@qq.com>
2025-09-03 12:13:36 +0800

    init: 初始化 spring-boot-course 项目

---
+++ src/test/java/top/dwy/boot/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dwy.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
