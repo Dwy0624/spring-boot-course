b21fa8c142df6b44085b2f3d150130c62922161c
dwy <1554642647@qq.com>
2025-09-17 14:55:07 +0800

    feat:新增了UserAccountController接口，读取自定义配置

---
+++ spring-boot-mp/src/main/java/top/dwy/boot/mp/MpApplication.java
@@ -0,0 +1,18 @@
+package top.dwy.boot.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DWY
+ */
+@SpringBootApplication
+public class MpApplication {
+    public static void main( String[] args )
+    {
+        SpringApplication.run(MpApplication.class,args);
+
+    }
+
+
+}
