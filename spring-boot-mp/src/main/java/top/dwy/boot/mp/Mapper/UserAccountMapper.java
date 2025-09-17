bb23e6773cb48437730143fdf0b3a0f1d7d9dfe9
dwy <1554642647@qq.com>
2025-09-17 14:52:36 +0800

    feat:新增了UserAccountController接口，读取自定义配置

---
+++ spring-boot-mp/src/main/java/top/dwy/boot/mp/Mapper/UserAccountMapper.java
@@ -0,0 +1,12 @@
+package top.dwy.boot.mp.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.dwy.boot.mp.entity.UserAccount;
+/**
+ * @author dwy
+ */
+@Mapper
+public interface UserAccountMapper extends BaseMapper<UserAccount> {
+
+}
